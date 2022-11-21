4103d2ed6dd996a1a8d847b38f6b44e043a1d870
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-11-21 22:02:33 +0800

    feat：添加注册相关接口

--- src/api/login/Register.api.ts
+++ src/api/login/Register.api.ts
@@ -1,28 +1,49 @@
 /*
  * @Author: xuan
- * @LastEditors: xuan
+ * @LastEditors: taozhiyaoyao
  * @Date: 2022-11-16 23:17:38
- * @LastEditTime: 2022-11-21 09:35:54
+ * @LastEditTime: 2022-11-21 21:59:40
  * @FilePath: \gcuwomd-oa\src\api\login\Register.api.ts
- * @Description: 
+ * @Description:
  */
-import { AxiosResponse } from "axios";
-import { Iregister } from "../../types/register";
-import { baseAxios } from "../axiosInst";
+import { AxiosResponse } from 'axios';
+import { IResponse } from '../../types/commomResp';
+import {
+  IOrganization,
+  IRegisterForm,
+  IRegisterInfo,
+} from '../../types/register';
+import { baseAxios } from '../axiosInst';
 
 const baseURL: string = 'http://127.0.0.1:4523/m1/1885531-0-default/';
 
+/**
+ * @description: 注册接口
+ */
 export const registerApi = (
-    url:string,
-    data: Iregister
-):Promise<AxiosResponse>=>{
-    return baseAxios.post(url,data,{
-        baseURL
-    })
+  url: string,
+  data: IRegisterForm
+): Promise<AxiosResponse<IResponse>> => {
+  return baseAxios.post(url, data, {
+    baseURL,
+  });
 };
-export const regirteroptions = (
-    geturl:string,
-    // getdata:string
-):Promise<AxiosResponse> =>{
-    return baseAxios.get(geturl,{baseURL})
-}
+
+/**
+ * @description: 获取组织列表
+ */
+export const getOrgListApi = (
+  url: string
+): Promise<AxiosResponse<IResponse<Array<IOrganization>>>> => {
+  return baseAxios.get(url, { baseURL });
+};
+
+/**
+ * @description: 根据学号查询姓名性别
+ */
+export const queryUserApi = (
+  url: string,
+  uid: string
+): Promise<AxiosResponse<IResponse<IRegisterInfo>>> => {
+  return baseAxios.get(url, { params: { uid }, baseURL });
+};
