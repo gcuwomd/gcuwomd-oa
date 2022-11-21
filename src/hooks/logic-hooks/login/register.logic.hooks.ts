b26b21525453ff1cfd8c1bb11e4ae9b21faf44dc
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-11-21 22:05:02 +0800

    feat：完善注册页功能

--- src/hooks/logic-hooks/login/register.logic.hooks.ts
+++ src/hooks/logic-hooks/login/register.logic.hooks.ts
@@ -1,30 +1,44 @@
 /*
  * @Author: xuan
- * @LastEditors: xuan
+ * @LastEditors: taozhiyaoyao
  * @Date: 2022-11-16 17:26:36
- * @LastEditTime: 2022-11-20 13:15:35
+ * @LastEditTime: 2022-11-21 21:59:49
  * @FilePath: \gcuwomd-oa\src\hooks\logic-hooks\login\register.logic.hooks.ts
- * @Description: 
+ * @Description:
  */
-import { AxiosResponse } from "axios";
-import { registerApi,regirteroptions } from "../../../api/login/Register.api";
-import { Iregister } from "../../../types/register";
+import { AxiosResponse } from 'axios';
+import { MessageApi } from 'naive-ui';
+import {
+  registerApi,
+  getOrgListApi,
+  queryUserApi,
+} from '../../../api/login/Register.api';
+import { IResponse } from '../../../types/commomResp';
+import {
+  IOrganization,
+  IRegisterForm,
+  IRegisterInfo,
+} from '../../../types/register';
 
-export const register = (
-    FormData:Iregister
-)=>{
-    return registerApi('sys/register',FormData).then((res)=>{
-        console.log('1');
-        
-    })
-}
-export const registerget = (
-)=>{
-    return regirteroptions('sys/getOrgList')
+export const register = async (FormData: IRegisterForm): Promise<IResponse> => {
+  const res = await registerApi('sys/register', FormData);
+  return res.data;
+};
+export const loadOrgOptions = async (orgList: Array<IOrganization>) => {
+  const res = await getOrgListApi('sys/getOrgList');
+  Object.assign(orgList, res.data.result);
+};
 
-
-
-
-    
-
-}
+export const loadUserInfo = async (
+  uid: string,
+  user: IRegisterInfo,
+  message: MessageApi
+) => {
+  const res = await queryUserApi('/sys/getUserInfoById', uid);
+  if (res.data.success) {
+    Object.assign(user, res.data.result);
+  } else {
+    Object.assign(user, { userName: '小樱', sex: 0 });
+    message.error(res.data.message);
+  }
+};
--- src/hooks/ui-hooks/login/register.ui.hooks.ts
+++ src/hooks/ui-hooks/login/register.ui.hooks.ts
@@ -1,19 +1,26 @@
 /*
  * @Author: xuan
- * @LastEditors: xuan
+ * @LastEditors: taozhiyaoyao
  * @Date: 2022-11-16 17:26:12
- * @LastEditTime: 2022-11-21 14:42:44
+ * @LastEditTime: 2022-11-21 21:49:54
  * @FilePath: \gcuwomd-oa\src\hooks\ui-hooks\login\register.ui.hooks.ts
  * @Description:
  */
 import { onMounted, reactive, ref } from 'vue';
 import { FormInst, MessageApi } from 'naive-ui';
-import { Ioptions, Iregister } from '../../../types/register';
+import {
+  IDepartment,
+  Ioptions,
+  IOrganization,
+  IRegisterForm,
+  IRegisterInfo,
+} from '../../../types/register';
 import {
   register,
-  registerget,
+  loadOrgOptions,
+  loadUserInfo,
 } from '../../logic-hooks/login/register.logic.hooks';
-import { Value } from 'sass';
+import { SelectBaseOption } from 'naive-ui/es/select/src/interface';
 
 export const org: any = ref<object>({
   label: null,
@@ -21,65 +28,49 @@ export const org: any = ref<object>({
 });
 export const dpt = ref<object>();
 
-export const orgOptions = 
-  registerget().then((res:any) => {
-    console.log(res.data);
-    org.value = res.data.result.map((item: { orgName: any; orgCode: any }) => ({
-      label: item.orgName,
-      value: item.orgCode,
-    }));
-    console.log(org.value);
-    
-  
+export const orgList: Array<IOrganization> = reactive([]);
+
+/**
+ * @description: 加载组织列表
+ */
+loadOrgOptions(orgList);
+
+export const departmentList = ref<Array<IDepartment>>();
+
+export const renderDepList = (orgCode: string): void => {
+  RegisterForm.depCode = null;
+  orgList.map((org) => {
+    if (org.orgCode === orgCode) {
+      departmentList.value = org.departments;
+    }
   });
-
-
-
-export const dptOptions = [
-  {
-    label: 'xzmsb',
-    value: 'xzmsb',
-  },
-  {
-    label: 'wlywb',
-    value: 'wlywb',
-  },
-  {
-    label: 'wzywb',
-    value: 'wzywb',
-  },
-  {
-    label: 'xxh',
-    value: 'xxh',
-  },
-];
+};
 
 export const RegisterFormRules = {
-  organization: {
-    require: true,
+  orgCode: {
+    required: true,
     message: '请选择组织',
     trigger: ['blur', 'change'],
   },
-  department: {
-    type: 'array',
-    require: true,
+  depCode: {
+    required: true,
     message: '请选择部门',
     trigger: ['blur', 'change'],
   },
   uid: {
-    require: true,
+    required: true,
     message: '请输入学号',
     trigger: 'blur',
   },
   phone: {
-    require: true,
+    required: true,
     message: '请输入联系电话',
     trigger: 'blur',
   },
 };
-export const RegisterForm: Iregister = reactive({
-  organization: null,
-  department: null,
+export const RegisterForm: IRegisterForm = reactive({
+  orgCode: null,
+  depCode: null,
   uid: null,
   phone: null,
 });
@@ -89,14 +80,25 @@ export const clearForm = (): void => {
   });
 };
 
-export const handleregister = (ref: FormInst, message: MessageApi): void => {
-  ref?.validate(async (errors) => {
+export const handleRegister = (ref: FormInst, message: MessageApi): void => {
+  ref.validate((errors) => {
     if (!errors) {
-      register(RegisterForm).then(() => {
-        message.success('注册成功');
+      register(RegisterForm).then((res) => {
+        console.log(res);
       });
     } else {
-      message.error('11111');
+      message.error('请把表单信息填写完整');
     }
   });
+};
+
+export const resModal = ref<boolean>(false);
+
+export const registerInfo: IRegisterInfo = reactive({
+  userName: null,
+  sex: 0,
+});
+
+export const renderUserInfo = (message: MessageApi): void => {
+  loadUserInfo(RegisterForm.uid as string, registerInfo, message);
 };
